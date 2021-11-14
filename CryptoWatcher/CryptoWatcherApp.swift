4d1f9b61d018adb1aee4889058d26973ed6b6c98
Roland Mikhel <Roolli9705@gmail.com>
2021-11-14 20:31:14 +0100

    Initial Commit

---
+++ CryptoWatcher/CryptoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWatcherApp.swift
+//  CryptoWatcher
+//
+//  Created by roland mikhel on 2021. 11. 14..
+//
+
+import SwiftUI
+
+@main
+struct CryptoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
